45f1ddd1a39f130afed3f1f908c622be866d6742
Andy Wang <xw214@duke.edu>
2024-04-18 23:02:25 -0400

    refactor

--- model/features/genetics_features.py
+++ model/features/genetics_features.py
@@ -10,14 +10,18 @@ class GeneticCNN2D(nn.Module):
 
     def __init__(self, length:int, class_count:int, include_connected_layer:bool, remove_last_layer:bool=False):
         super().__init__()
+        
+        self.length = length
+        self.class_count = class_count
+        self.include_connected_layer = include_connected_layer
+        self.remove_last_layer = remove_last_layer
+        
         self.conv1 = nn.Conv2d(4, 16, kernel_size=(1,3), padding=(0,1))
         self.conv2 = nn.Conv2d(16, 32, kernel_size=(1,3), padding=(0,1))
         self.conv3 = nn.Conv2d(32, 64, kernel_size=(1,3), padding=(0,1))
         # self.conv4 = nn.Conv2d(64, 128, kernel_size=(1,3), padding=(0,1))
         self.pool = nn.MaxPool2d(kernel_size=(1,2), stride=2)
         self.pool3 = nn.MaxPool2d(kernel_size=(1,3), stride=3)
-
-        self.remove_last_layer = remove_last_layer
 
         if include_connected_layer:
             self.fc1 = nn.Linear(64 * (length // 18), class_count)
@@ -34,3 +38,21 @@ class GeneticCNN2D(nn.Module):
             x = self.fc1(x)
             # return F.log_softmax(x, dim=1)
         return x
+    
+    
+    def __repr__(self):
+        rep = {
+            'GENETICS(\n'
+            '\tlength: {},\n'
+            '\tclass_count: {},\n'
+            '\tinclude_connected_layer: {},\n'
+            '\tremove_last_layer: {},\n'
+            ')'
+        }
+        
+        return rep.format(
+            self.length,
+            self.class_count,
+            self.include_connected_layer, 
+            self.remove_last_layer
+        )
